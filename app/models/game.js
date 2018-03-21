6ff0222026e28457ce6dbcb5b022dc0762936cfa
ex7 <extrim7@gmail.com>
2018-03-21 10:28:33 +0500

    models added

---
+++ app/models/game.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  order: DS.attr('number'),
+  scoreP1: DS.attr('number'),
+  scoreP2: DS.attr('number')
+});
---
+++ app/models/match.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  startDate: DS.attr('date'),
+  player1: DS.belongsTo('player'),
+  player2: DS.belongsTo('player'),
+  scoreP1: DS.attr('number'),
+  scoreP2: DS.attr('number')
+});
