c5e8a8710ef91ce09124caec8d13d4ffe0074713
Pavel B <extrim7@gmail.com>
2018-03-22 14:30:59 +0500

    app and match templates

--- app/routes/match.js
+++ app/routes/match.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
 
-let matches = [
-  {
-    name: 'First',
-    startDate: new Date(2018, 03, 18),
-    scoreP1: null,
-    scoreP2: null
-  },
-  {
-    name: 'Second',
-    startDate: new Date(2018, 03, 20),
-    scoreP1: null,
-    scoreP2: null
-  },
-];
-
 export default Ember.Route.extend({
   model() {
-    return matches[0];
+    return this.get('store').createRecord('match');
   }
 });
