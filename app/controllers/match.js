8814ff8bbd52273b4a757febde7fc8c8bdeb4563
Pavel B <extrim7@gmail.com>
2018-04-02 20:48:56 +0500

    eval Elo rating on form AND eslint correction

--- app/controllers/match.js
+++ app/controllers/match.js
@@ -1,15 +1,30 @@
 import Ember from 'ember';
+import ratingElo from '../utils/rating-elo';
 
 export default Ember.Controller.extend({
-  init() {
-    this._super(...arguments);
+  init(...args) {
+    this._super(args);
     this.set('players', this.get('store').peekAll('player'));
   },
   actions: {
     addOneGame() {
-      var g = this.get('store').createRecord('game', {
-        order: this.get('model.games.length') + 1
+      const g = this.get('store').createRecord('game', {
+        order: this.model.get('games.length') + 1
       });
+      let [r1, r2] = [this.model.get('player1.rating'), this.model.get('player2.rating')];
+
+      this.model.get('games').forEach(element=> {
+        let scoreElo = 0;
+
+        if (element.scoreP1 > element.scoreP2) {
+          scoreElo = 1;
+        } else if (element.scoreP1 < element.scoreP2) {
+          scoreElo = -1;
+        }
+        [r1, r2] = ratingElo(r1, r2, scoreElo);
+      });
+      this.model.set('player1.rating', r1);
+      this.model.set('player2.rating', r2);
       this.model.get('games').pushObject(g);
     },
     toggleSettings() {
--- app/utils/rating-elo.js
+++ app/utils/rating-elo.js
@@ -5,29 +5,30 @@ const defaultK = [
 ];
 
 /**
-* @param {number} rating1
-* @param {number} rating2
+* @param {number} rating1 рейтинг игрока1
+* @param {number} rating2 рейтинг игрока2
 * @param {number} score 1: выиграл первый, 0: ничья, -1: выиграл второй
 * @returns {any} Вернет массив новых рейтинов
 */
 export default function ratingElo(rating1, rating2, score) {
-  let scoreFn = x => { return x > 0 ? 1 : (x < 0 ? 0 : 0.5 ); };
-  let r1 = calcRating(rating1, rating2, scoreFn(score) );
-  let r2 = calcRating(rating2, rating1, scoreFn(-score));
+  const scoreFn = x=> { return x > 0 ? 1 : (x < 0 ? 0 : 0.5); };
+  const r1 = calcRating(rating1, rating2, scoreFn(score));
+  const r2 = calcRating(rating2, rating1, scoreFn(-score));
+
   return [r1, r2];
 }
 
 /**
-* @param {number} rating1
-* @param {number} rating2
+* @param {number} rating1 рейтинг игрока1
+* @param {number} rating2 рейтинг игрока2
 * @param {number} scoreFact 1 очко за победу, 0,5 — за ничью и 0 — за поражение
-* @returns {number}
+* @returns {number} новый рейтинг
 */
-function calcRating(rating1, rating2, scoreFact)
-{
-  let ea = 1 / ( 1 + 10 * (rating2 - rating1)/400 );
-  let elm = defaultK.filter(item => { return item.ratingMin <= rating1 && item.ratingMax > rating1; });
-  let k = elm ? elm[0].k : 40;
-  let newRating = rating1 + k * ( scoreFact - ea );
+function calcRating(rating1, rating2, scoreFact) {
+  const ea = 1 / (1 + 10 * (rating2 - rating1) / 400);
+  const elm = defaultK.filter(item=> { return item.ratingMin <= rating1 && item.ratingMax > rating1; });
+  const k = elm ? elm[0].k : 40;
+  const newRating = rating1 + k * (scoreFact - ea);
+
   return newRating;
 }
