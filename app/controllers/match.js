23d52252d28c254fa6ed6a766c2b0e83070548d3
Pavel B <extrim7@gmail.com>
2018-04-03 12:05:17 +0500

    removed forEach when calc rating

--- app/controllers/match.js
+++ app/controllers/match.js
@@ -8,24 +8,31 @@ export default Ember.Controller.extend({
   },
   actions: {
     addOneGame() {
+      // рассчитаем изменение рейтинга
+      let [r1, r2] = [this.model.get('player1.rating'), this.model.get('player2.rating')];
+      const lastGame = this.model.get('games.lastObject');
+      let scoreElo = 0;
+
+      if (lastGame === undefined || lastGame.get('scoreP1') === undefined || lastGame.get('scoreP2') === undefined) {
+        return false;
+      }
+      if (lastGame.scoreP1 > lastGame.scoreP2) {
+        scoreElo = 1;
+      } else if (lastGame.scoreP1 < lastGame.scoreP2) {
+        scoreElo = -1;
+      }
+      [r1, r2] = ratingElo(r1, r2, scoreElo);
+
+      this.model.set('player1.rating', r1);
+      this.model.set('player2.rating', r2);
+      // добавим новую игру
       const g = this.get('store').createRecord('game', {
         order: this.model.get('games.length') + 1
       });
-      let [r1, r2] = [this.model.get('player1.rating'), this.model.get('player2.rating')];
 
-      this.model.get('games').forEach(element=> {
-        let scoreElo = 0;
-
-        if (element.scoreP1 > element.scoreP2) {
-          scoreElo = 1;
-        } else if (element.scoreP1 < element.scoreP2) {
-          scoreElo = -1;
-        }
-        [r1, r2] = ratingElo(r1, r2, scoreElo);
-      });
-      this.model.set('player1.rating', r1);
-      this.model.set('player2.rating', r2);
       this.model.get('games').pushObject(g);
+
+      return false;
     },
     toggleSettings() {
       this.toggleProperty('isExpanded');
